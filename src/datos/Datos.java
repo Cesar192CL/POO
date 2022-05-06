afb924b8589b93a8449f5d975798600af4ff745e
Hp <Hp@DESKTOP-K8E7R0P>
2022-05-05 22:46:16 -0600

    Se creo proyecto 0.01 / Actualizado info

--- src/datos/Datos.java
+++ src/datos/Datos.java
@@ -1,6 +1,7 @@
 /*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+Cesar Alejandro Cervnates Legarreta
+21550871
+
  */
 package datos;
 
